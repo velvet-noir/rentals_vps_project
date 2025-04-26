2108d31b3f023a86857de99d206d93c408f1fa77
velvet-noir <this.is.romniko@gmail.com>
2025-04-26 19:54:53 +0700

    Add models Service, ServiceSpecification

--- backend_api/admin.py
+++ backend_api/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Service, ServiceSpecification
 
-# Register your models here.
+
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ("name", "image", "price", "is_active")
+    ordering = ("id",)
+
+class ServiceSpecificationAdmin(admin.ModelAdmin):
+    list_display = ("get_service_name", "processor", "ram", "disk", "internet_speed")
+    ordering = ("id",)
+
+    def get_service_name(self, obj):
+        return f"Характеристика: {obj.service.name}"
+    
+
+admin.site.register(Service, ServiceAdmin)
+admin.site.register(ServiceSpecification, ServiceSpecificationAdmin)
+
--- backend_api/models.py
+++ backend_api/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+
+class Service(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.CharField(max_length=200)
+    mini_description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["id"]
+        verbose_name = "Услуга"
+        verbose_name_plural = "Услуги"
+
+
+class ServiceSpecification(models.Model):
+    service = models.ForeignKey(
+        Service, on_delete=models.CASCADE, related_name="specifications"
+    )
+    description = models.TextField()
+    processor = models.CharField(max_length=100)
+    ram = models.CharField(max_length=100)
+    disk = models.CharField(max_length=100)
+    internet_speed = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.service.name
+
+    class Meta:
+        ordering = ["id"]
+        verbose_name = "Характеристика услуги"
+        verbose_name_plural = "Характетистики услуг"
