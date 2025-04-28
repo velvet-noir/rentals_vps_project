2ffe15c8a3e781437e26d5dc410b1cf54ded5cc4
velvet-noir <this.is.romniko@gmail.com>
2025-04-28 19:17:19 +0700

    add view & fix templates

--- backend_api/urls.py
+++ backend_api/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path("", views.service_list, name="service_list"),
+    path("", views.service_list, name="service_list"),
+    path("service/<int:pk>/", views.service_detail, name="service_detail"),
+    path("service/<int:pk>/delete/", views.service_delete, name="service_delete"),
 ]
--- backend_api/views.py
+++ backend_api/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
+from django.db.models import Q
+from .models import Service
 
-# Create your views here.
+
+def service_list(request):
+    query = request.GET.get("query", "")
+
+    services = Service.objects.filter(is_active=True)
+
+    if query:
+        services = services.filter(
+            Q(name__icontains=query) | Q(mini_description__icontains=query)
+        )
+
+    return render(
+        request,
+        "backend_api/service_list.html",
+        {
+            "services": services,
+            "query": query,
+        },
+    )
+
+
+def service_detail(request, pk):
+    service = get_object_or_404(Service, pk=pk)
+
+    context = {
+        "service": service,
+    }
+    return render(request, "backend_api/service_detail.html", context)
+
+
+def service_delete(request, pk):
+    service = get_object_or_404(Service, pk=pk)
+
+    if request.method == 'POST':
+        service.is_active = False
+        service.save()
+
+        return redirect('service_list')
+
+    return redirect('service_list')
